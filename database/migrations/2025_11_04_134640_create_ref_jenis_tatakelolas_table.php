a847e6277ca9661e068a3de334463a33e293c0d5
tbimantara04-hub <tbimantara04@gmail.com>
2025-11-04 20:47:59 +0700

    Add create_ref_jenis_tatakelolas_table migration

--- database/migrations/2025_11_04_134640_create_ref_jenis_tatakelolas_table.php
+++ database/migrations/2025_11_04_134640_create_ref_jenis_tatakelolas_table.php
@@ -9,16 +9,13 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-  public function up(): void
-{
-    Schema::create('ref_jenis_tatakelolas', function (Blueprint $table) {
-        $table->id();
-        // TAMBAHKAN KOLOM ANDA DI SINI
-        $table->string('nama'); // <-- Contoh
-        $table->text('deskripsi')->nullable(); // <-- Contoh
-        $table->timestamps();
-    });
-}
+    public function up(): void
+    {
+        Schema::create('ref_jenis_tatakelolas', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
 
     /**
      * Reverse the migrations.
