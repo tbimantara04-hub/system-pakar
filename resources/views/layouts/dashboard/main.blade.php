764010198091f153b6b7b3e219728d4a4546f102
tbimantara04-hub <tbimantara04@gmail.com>
2025-12-17 09:23:20 +0700

    Remove duplicate jQuery include to fix bootstrap conflicts

--- resources/views/layouts/dashboard/main.blade.php
+++ resources/views/layouts/dashboard/main.blade.php
@@ -33,7 +33,7 @@
 
 <script src="{{ asset('dist/js/vendor.js') }}"></script>
 <script src="{{ asset('dist/js/adminx.js') }}"></script>
-<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 
 @stack('scripts')
