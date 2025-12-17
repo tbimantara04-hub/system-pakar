a395b10fea83dc97cd1adf1eb04ee4364522c204
tbimantara04-hub <tbimantara04@gmail.com>
2025-12-17 10:12:25 +0700

    Add Bootstrap 4 bundle to fix sidebar collapse functionality

--- resources/views/layouts/dashboard/main.blade.php
+++ resources/views/layouts/dashboard/main.blade.php
@@ -32,6 +32,7 @@
 </div>
 
 <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
 <script src="{{ asset('dist/js/vendor.js') }}"></script>
 <script src="{{ asset('dist/js/adminx.js') }}"></script>
 
